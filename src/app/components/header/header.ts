0d925c793f9aec6e9a93f6c8a0a7a63b8b615643
rosana <rosmarielnunez@gmail.com>
2025-09-19 16:37:42 +0200

    Delete Tailwind and add Angular Material 3

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
